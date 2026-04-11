c9f491dafda795c7a995db04673cd4fad3981844
Kim YeJin <320418@naver.com>
2026-04-11 23:54:44 +0900

    0411

---
+++ CHAPTER 03 그리디/1/src/greedy1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class greedy1 {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int pay = sc.nextInt();
+
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // br.readLine()으로 한 줄을 통째로 읽어온 뒤 int로 변환
+//    int pay = Integer.parseInt(br.readLine());
+
+    int[] coins = {500, 100, 50, 10};
+    int result = 0;
+
+    for (int coin : coins) {
+      result += pay / coin;
+      pay %= coin;
+    }
+
+    System.out.println(result);
+  }
+}
