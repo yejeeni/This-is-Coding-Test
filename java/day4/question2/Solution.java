e7d678d7221b75999e04cc3a16609b156ba7f4b6
Kim YeJin <320418@naver.com>
2026-05-14 01:28:45 +0900

    day4

---
+++ java/day4/question2/Solution.java
@@ -0,0 +1,71 @@
+package day4.question2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+
+    // 1. HashMap으로 이름:점수 저장하고 정렬하기
+    HashMap<String, Integer> nameScore = new HashMap<>();
+    for (int i=0; i<n; i++) {
+      st = new StringTokenizer(br.readLine());
+      // name: score 저장
+      nameScore.put(st.nextToken(), Integer.parseInt(st.nextToken()));
+    }
+
+    // HashMap에서 entry 목록을 ArrayList로 꺼내기
+    ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>(nameScore.entrySet());
+
+    // 점수 내림차순, 동점이면 이름 오름차순으로 정렬
+    list.sort((a, b) -> {
+      if (!a.getValue().equals(b.getValue())) { // 두 점수가 다를 경우
+        return b.getValue() - a.getValue(); // 점수 내림차순
+      }
+      return a.getKey().compareTo(b.getKey()); // 이름 문자 compareTo()로 비교하고 오름차순
+    });
+
+    st = new StringTokenizer(br.readLine());
+    int q = Integer.parseInt(st.nextToken());
+
+    for (int i=0; i<q; i++) {
+      st = new StringTokenizer(br.readLine());
+      if (st.nextToken().equals("rank")) {
+//      rank name — name의 등수를 출력 (동점자가 있으면 같은 등수, 등수는 나보다 점수 높은 사람 수 + 1)
+        String name = st.nextToken();
+        Integer score = nameScore.get(name);
+
+        int rank = 0;
+        for (int j=0; j<list.size(); j++) {
+          if (list.get(j).getValue() > score) { // 정렬해둔 목록에서 나보다 점수 높은 사람이 있는 경우
+            rank++;
+          }
+        }
+
+        System.out.println(rank+1);
+      } else {
+//      top K — 점수 상위 K명의 이름을 점수 내림차순으로 출력 (동점이면 이름 오름차순)
+        int k = Integer.parseInt(st.nextToken());
+
+        for (int j=0; j<k; j++) {
+          System.out.print(list.get(j).getKey());
+          if (j != k-1) {
+            System.out.print(" ");
+          }
+        }
+      }
+    }
+  }
+}
---
+++ java/day4/question3/Solution.java
@@ -0,0 +1,51 @@
+package day4.question3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());
+
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder()); // 중앙값 이하 숫자 모음. 큰 순으로 우선순위라 top이 항상 중앙값임
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // 중앙값 초과 숫자 모음. 작은 것 우선순위. 작은 순으로 우선순위라 top은 중앙값보다 한 단계 더 큰 수
+
+    st = new StringTokenizer(br.readLine());
+    for (int i=0; i<n; i++) {
+      int num = Integer.parseInt(st.nextToken());
+
+//  // 처음 값 들어오면 걍 max힙에 저장
+      if (maxHeap.isEmpty()) {
+        maxHeap.offer(num);
+      } else {
+        if (maxHeap.peek() > num) { // 중앙값보다 크면
+          maxHeap.offer(num);
+        } else {
+          minHeap.offer(num);
+        }
+      }
+
+//  // 개수정리
+      while (maxHeap.size() < minHeap.size()) {
+        maxHeap.offer(minHeap.poll());
+      }
+      while (maxHeap.size() > minHeap.size() + 1) {  // maxHeap이 2 이상 크면
+        minHeap.offer(maxHeap.poll());
+      }
+      //print(maxHeap 마지막원소)
+      if (i > 0) {
+        sb.append(" ");
+      }
+      sb.append(maxHeap.peek());
+    }
+    System.out.println(sb);
+  }
+}
